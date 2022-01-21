bbecf3745fd626cb711748169bb7f9d7a5c2c93a
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2022-01-21 11:31:30 +0900

    🎨 Add rules to fix for Lerna bootstrap behavior

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,16 @@
 module.exports = {
   arrowParens: 'avoid',
   semi: true,
-  trailingComma: 'none'
+  trailingComma: 'none',
+  overrides: [
+    // TODO: this is temporary due to lerna/npm v8 issue
+    // https://github.com/lerna/lerna/issues/2845
+    {
+      files: 'packages/*/package-lock.json',
+      options: {
+        tabWidth: 4,
+        useTabs: true
+      }
+    }
+  ]
 };
