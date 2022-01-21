e3486e53acd8ec3f064bfa6127161fadbd38f304
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2022-01-21 11:40:11 +0900

    🔧 Add pattern in lint-staged for reformat package-lock.json

--- packages/apps/.lintstagedrc.js
+++ packages/apps/.lintstagedrc.js
@@ -2,5 +2,8 @@ module.exports = {
   '**/*.{js,jsx,ts,tsx}': filenames => [
     `eslint --fix ${filenames.join(' ')}`,
     `prettier --write ${filenames.join(' ')}`
+  ],
+  'packages/*/package.json': () => [
+    'prettier --write packages/*/package-lock.json'
   ]
 };
