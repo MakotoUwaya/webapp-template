9a4d3e4f2bb7a4992631b90e509afdd9ce295873
oichan <makoto.uwaya@gmail.com>
2022-03-20 18:20:12 +0900

    📦️ Install Vuetify 3

--- packages/vue3-vite/src/main.ts
+++ packages/vue3-vite/src/main.ts
@@ -2,11 +2,15 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
 import router from './router';
 
-const app = createApp(App);
+loadFonts();
 
+const app = createApp(App);
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount('#app');
