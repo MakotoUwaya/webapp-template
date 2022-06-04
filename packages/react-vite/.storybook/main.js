8e2a5a1b51545ab31ddced97d2fa671c6a119d0c
oichan <makoto.uwaya@gmail.com>
2022-06-04 16:11:12 +0900

    �️ Convert Storybook config into Typescript

--- packages/react-vite/.storybook/main.js
+++ packages/react-vite/.storybook/main.js
@@ -1,16 +1,5 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app'
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite'
-  },
-  features: {
-    storyStoreV7: true
-  }
-};
+const { register } = require('esbuild-register/dist/node');
+register({
+  target: 'node16'
+});
+module.exports = require('./main.ts');
