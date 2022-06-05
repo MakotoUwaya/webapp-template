77b29d033da537f629d6f6a6a31c5f1635dab66d
oichan <makoto.uwaya@gmail.com>
2022-06-05 10:16:04 +0900

    � Update config for storybook

--- packages/react-vite/.storybook/main.ts
+++ packages/react-vite/.storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookViteConfig } from '@storybook/builder-vite';
 import { mergeConfig } from 'vite';
 
 const config: StorybookViteConfig = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-a11y',
     '@storybook/addon-links',
@@ -17,11 +17,9 @@ const config: StorybookViteConfig = {
   features: {
     storyStoreV7: true
   },
-  viteFinal(config, options) {
+  async viteFinal(config, options) {
     return mergeConfig(config, {
-      // Use the same "resolve" configuration as your app
-      // resolve: (await import('../vite.config.ts')).default,
-      // Add dependencies to pre-optimization
+      resolve: (await import('../vite.config')).default,
       optimizeDeps: {
         include: ['storybook-dark-mode']
       }
