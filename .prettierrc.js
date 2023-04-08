1902f9c7aaa0602feb6bc09f905b198994aac15b
oichan <makoto.uwaya@gmail.com>
2023-04-08 21:00:26 +0900

    🔥 Remove lerna

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,15 +2,4 @@ module.exports = {
   arrowParens: 'avoid',
   semi: true,
   trailingComma: 'none',
-  overrides: [
-    // TODO: this is temporary due to lerna/npm v8 issue
-    // https://github.com/lerna/lerna/issues/2845
-    {
-      files: 'packages/*/package-lock.json',
-      options: {
-        tabWidth: 4,
-        useTabs: true
-      }
-    }
-  ]
 };
