617ad6d4bbd5db078ad86b9660c65d2856931d60
oichan <makoto.uwaya@gmail.com>
2024-01-13 20:45:59 +0900

    Remove ESLint settings

--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  arrowParens: 'avoid',
-  semi: true,
-  trailingComma: 'none',
-};
--- packages/vue3-vite/lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '**/*.{js,jsx,ts,tsx}': filenames => [
-    `eslint --fix ${filenames.join(' ')}`,
-    `prettier --write ${filenames.join(' ')}`
-  ]
-};
