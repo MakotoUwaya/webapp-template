b5bf639796e0d666725520fffca24f72678ff00c
oichan <makoto.uwaya@gmail.com>
2025-11-15 20:58:26 +0900

    chore(deps): add @testing-library/dom v10.4.1 to react-vite package and update base.css font-family formatting

--- packages/react-vite/src/about/index.test.tsx
+++ packages/react-vite/src/about/index.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 
 test('about render', () => {
   // TODO: Fix About component test
-  // biome-ignore lint/complexity/noUselessFragments: https://github.com/testing-library/react-testing-library/issues/1371
   render(<>Vite?</>);
   const linkElement = screen.getByText(/Vite\?/i);
   expect(linkElement).toBeInTheDocument();
